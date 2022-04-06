8b71efc7765a099ac57f71d51d3b7f7b7023a061
Olubunmi Bakre <bakreolubunmi@yahoo.com>
2022-04-06 16:55:45 +0100

    python journey

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+s = "spam"
+new_s = s[0:1] + 'l' + s[2:4]
+print(new_s)
+print(s)
+my_str = "Hello World"
+var = "e" in my_str
+print(var)
+
+a = 4
+b = 7
+print(a + b)
+
+a = '4'
+b = '9'
+print(a + b)
+
 user_input = int(input('Enter number between 1 and 100: '))
 while user_input != 1:
     if user_input % 2 != 0:
@@ -12,3 +28,8 @@ if my_str == var:
     print("Palindrome")
 else:
     print("Not palindrome")
+
+for i in range(11, 1, -1):
+    print('*' * i)
+for i in range(1, 11):
+    print('*' * i)
