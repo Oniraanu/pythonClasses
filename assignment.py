3aff89d72987fb915be43ed28a0ed7d9718f1f1b
Olubunmi Bakre <bakreolubunmi@yahoo.com>
2022-04-07 14:44:29 +0100

    python journey

--- assignment.py
+++ assignment.py
@@ -1,2 +1,25 @@
-# fibonacci sequence
+# add numbers
+sum = 0
+total = 1
+user_input = int(input("Enter number: "))
+for i in range(1, user_input + 1):
+    sum = sum + 1
+    total = sum + 1
+    print(total)
+
+
 # factorial
+import math
+
+user_input = int(input("Enter number: "))
+answer = math.factorial(user_input)
+print(answer)
+
+# fibonacci sequence
+user_input = int(input("Enter number: "))
+a, b = 0, 1
+while a < user_input:
+    print(a)
+    a, b = b, a + b
+
+
--- python_lessons.py
+++ python_lessons.py
@@ -1,35 +1,72 @@
-s = "spam"
-new_s = s[0:1] + 'l' + s[2:4]
-print(new_s)
-print(s)
-my_str = "Hello World"
-var = "e" in my_str
-print(var)
+# s = "spam"
+# new_s = s[0:1] + 'l' + s[2:4]
+# print(new_s)
+# print(s)
+# my_str = "Hello World"
+# var = "e" in my_str
+# print(var)
+#
+# a = 4
+# b = 7
+# print(a + b)
+#
+# a = '4'
+# b = '9'
+# print(a + b)
+#
+# user_input = int(input('Enter number between 1 and 100: '))
+# while user_input != 1:
+#     if user_input % 2 != 0:
+#         user_input = (user_input * 3) + 1
+#     else:
+#         user_input = user_input / 2
+#     print(user_input)
+#
+# my_str = input('Enter number: ')
+# var = my_str[::-1]
+# if my_str == var:
+#     print("Palindrome")
+# else:
+#     print("Not palindrome")
+#
+# for i in range(11, 1, -1):
+#     print('*' * i)
+# for i in range(1, 11):
+#     print('*' * i)
+# user_input = int(input("Enter: "))
+# a = range(1, user_input + 1)
+# print(len(a))
+#
+# x = "Hello"
+# print(len(x))
 
-a = 4
-b = 7
-print(a + b)
+# my_str = 'python rules!'
+# print(my_str.upper())
+# print(my_str.find('r'))
 
-a = '4'
-b = '9'
-print(a + b)
+# my_str = 'He had the bat'
+# my_str.count('t')
+# print(my_str.replace('t', 'd'))
 
-user_input = int(input('Enter number between 1 and 100: '))
-while user_input != 1:
-    if user_input % 2 != 0:
-        user_input = (user_input * 3) + 1
-    else:
-        user_input = user_input / 2
-    print(user_input)
+# str = "This is the world of python"
+# print(str.upper().count('T'))
+# print(str.replace('python', 'java'))
+# print(str.replace(' ', ' not ',1))
 
-my_str = input('Enter number: ')
-var = my_str[::-1]
-if my_str == var:
-    print("Palindrome")
-else:
-    print("Not palindrome")
+# print(str.title())
 
-for i in range(11, 1, -1):
-    print('*' * i)
-for i in range(1, 11):
-    print('*' * i)
+# print("1001001".translate(str.maketrans('01', '10')))
+# import string
+#
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(string.ascii_lowercase)
+#
+# print("73543635".translate(str.maketrans('3', '1')))
+# print("hello".translate(str.maketrans('o', ' ')) + "is here")
+
+str = 'hello'
+new_str = 'to you'
+print(f'Bunmi is saying {str}')
+print('Bunmi is saying {} and weldone {}'.format(str, new_str))
+print('Bunmi is saying {1} and weldone {0}'.format(str, new_str))
